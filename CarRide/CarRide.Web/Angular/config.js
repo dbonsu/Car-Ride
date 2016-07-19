93ddc426601df5bf8c3b5ca1fc7e30cee396bbf3
derick <dbonsu2012@gmail.com>
2016-07-18 22:28:56 -0400

    setting up angular

---
+++ CarRide/CarRide.Web/Angular/config.js
@@ -0,0 +1,14 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('carRide')
+        .config(CarRide);
+
+    CarRide.$inject = ['$stateProvider', '$urlRouterProvider', '$locationProvider', '$httpProvider'];
+    function CarRide($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
+        $urlRouterProvider.otherwise('/');
+
+        $stateProvider
+        .when();
+    };
+})()
